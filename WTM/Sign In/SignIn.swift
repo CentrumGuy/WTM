2682dfad211b65d83ebb7418bd49d23d6bec0aeb
Haris Hussain <hussainharis04@gatech.edu>
2018-11-04 04:21:56 -0500

    user authentication enabled

--- WTM/Sign In/SignIn.swift
+++ WTM/Sign In/SignIn.swift
@@ -7,17 +7,33 @@
 //
 
 import UIKit
+import Firebase
 
 class SignIn: UIViewController {
 
+    @IBOutlet weak var email: UITextFieldX!
+    @IBOutlet weak var ps: UITextFieldX!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         applyColorScheme(applyBG: true)
-        //Allows tap-out gesture 
+        //Allows tap-out gesture
+        
+        
         self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action:#selector(UIView.endEditing(_:))))
         
 
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func signIn(_ sender: Any) {
+        guard let email = email.text, !email.isEmpty, let pass = ps.text, !pass.isEmpty else {
+            return
+        }
+    }
+        
+    
     
 }
